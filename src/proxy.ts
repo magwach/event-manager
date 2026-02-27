a6cc0a4646aa55daf0689da58bd1268f42bb692c
magwach <emmanuelmagothe@gmail.com>
2026-02-27 09:57:32 +0300

    fix: Updated the middleware to accept the webhook.

--- src/proxy.ts
+++ src/proxy.ts
@@ -7,6 +7,7 @@ const isPublicRoute = createRouteMatcher([
   "/terms-of-service",
   "/sign-in(.*)",
   "/sign-up(.*)",
+  "/api/webhooks(.*)",
 ]);
 
 export default clerkMiddleware(async (auth, req) => {
